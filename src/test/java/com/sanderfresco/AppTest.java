1783a3df5b5665a222f66c2da1144b679bc5c55b
shrinidhi1996 <shrinidhigowda.17@gmail.com>
2025-08-26 12:59:42 +0530

    added unit test

---
+++ src/test/java/com/sanderfresco/AppTest.java
@@ -0,0 +1,12 @@
+package com.sandersfresco;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
+
